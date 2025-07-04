0bcf57aa6a4ab9359cd23d3772b1a0df78c51c83
EndravathKrishna <krishnaendravath@gmail.com>
2025-07-04 13:19:02 +0530

    added backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url="http://localhost:5000"
+  const url="https://tomato-backend-vrjl.onrender.com"
 
   return (
     <div>
